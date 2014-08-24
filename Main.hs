cad3e7535f04fcf179e1ee347246e5daff2be1a3
agocorona <agocorona@gmail.com>
2014-08-24 18:08:48 -0400

    demo

--- Main.hs
+++ Main.hs
@@ -5,7 +5,7 @@ import MFlow.Wai.Blaze.Html.All
 import Haste.Compiler
 import Data.Default
 import Prelude hiding (id,div,head)
-import qualified Data.List as L(head)
+import qualified Data.List as L
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import Data.String
@@ -18,12 +18,12 @@ import Data.Typeable
 import Data.Monoid
 import Text.Blaze.Html5.Attributes as At hiding (step,name)
 import qualified Data.Text.Lazy as TL
-
+import Control.Monad
 import Control.Shell
 
 import Debug.Trace
 
-(!>)= flip trace
+--(!>)= flip trace
 
 projects= "./examples/"
 
@@ -35,9 +35,6 @@ instance Serializable Examples where
 
 listExamples (Examples list)= list
 
-
-
-
 main= do
   indexList listExamples (map TL.pack )
   examples <- atomically $ newDBRef $ Examples ["example.hs","todo.hs","sumtwonumbers.hs"]
@@ -45,19 +42,17 @@ main= do
   setFilesPath projects
   let dontcare ="updateme"
   runNavigation "try" . transientNav $ do
+    page . pageFlow "try" $ do
+          Examples exampleList <- liftIO $ atomically $ readDBRef examples
+                                  `onNothing` error "examples empty"
 
-
-    Examples exampleList <- liftIO $ atomically $ readDBRef examples
-                         `onNothing` error "examples empty"
-
-    page  $ do
-          example <- b  "you can load also one of these examples "
-                     ++> firstOf[wlink e << e <++ " " | e <- exampleList]
+          example <- wform $ b  "you can load also one of these examples "
+                     ++> firstOf[submitButton e  <++ " " | e <- exampleList]
                      <|> return "none"
 
           extext <- if example /= "none" then liftIO $ TIO.readFile $ projects ++ example else return ""
 
-          (name,r) <- p <<< ( (,) <$> getString (Just "dontcare")
+          (name,r) <- wform $ p <<< ( (,) <$> getString (Just example)
                                   <*> getMultilineText extext <! [("style","width:100%;height:300")]
                         <++ br
                         <** submitButton "compile"
@@ -67,14 +62,18 @@ main= do
           let haskell=  T.unpack r
               hsfile =  name ++ ".hs"
           liftIO $ writeFile  (projects ++ hsfile) haskell
-          r <- liftIO . shell $ inDirectory projects $ genericRun "/app/.cabal/bin/hastec" [hsfile,"--output-html"] "" !> hsfile
---          r <- liftIO . shell $ inDirectory projects $ genericRun "/home/user/.cabal/bin/hastec" [hsfile,"--output-html"] "" !> hsfile
+
+          let html =   projects ++ name ++ ".html"
+          exist <- liftIO $ doesFileExist html
+          when exist . liftIO $ removeFile html
+          r <- liftIO . shell $ inDirectory projects $ genericRun "/app/.cabal/bin/hastec" [hsfile,"--output-html"] ""
+--          r <- liftIO . shell $ inDirectory projects $ genericRun "/home/user/.cabal/bin/hastec" [hsfile,"--output-html"] ""
           case r of
             Left errs -> fromStr ("*******Failure: not found hastec"++  errs) ++> empty
             Right (r,out,err) ->
               case r of
                   True  -> do
-                    liftIO $ atomically $ writeDBRef examples . Examples $ (name++".hs"):exampleList
+                    liftIO . atomically . writeDBRef examples . Examples . L.nub $ (name++".hs"):exampleList
                     (a  ! href  (fromString("/"++name++".html")) $ "execute") ++> empty
                   False -> fromStr err ++> empty
 
