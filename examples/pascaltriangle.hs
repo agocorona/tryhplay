44b90876264ecc43add071cc353a8a79407bca46
Alberto G. Corona <agocorona@gmail.com>
2014-09-02 21:58:18 +0200

    test

--- examples/pascaltriangle.hs
+++ examples/pascaltriangle.hs
@@ -9,7 +9,7 @@ main= runBody $ showpascal
 pascal = iterate (\row -> zipWith (+) ([0] ++ row) (row ++ [0])) [1] :: [[Int]]
 
 showpascal =do
- n <- getInt Nothing `fire` OnKeyUp <|> return 10
+ n <- getInt Nothing ! atr "size" "5" `fire` OnKeyUp <|> return 10
  wraw $ do
    p << ("Show " ++ show n ++ " rows of the Pascal triangle ")
    mconcat[p ! style "text-align:center" $ row | row <- take n pascal]
